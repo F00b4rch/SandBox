53d027f6732dc97748a0c524f5f8eac6785de5b1
F00b4rch <f00b4rch@gmail.com>
2017-06-17 13:56:00 +0200

    created main.go

---
+++ golang/go_web_monit/main.go
@@ -0,0 +1,59 @@
+// This program have two mains : 
+// Let's do it simple
+// - Monitore website with http get
+// - Monitore host with ICMP
+package main
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+)
+
+// Define good answer from get
+const okStatus int = 200
+
+func main() {
+
+	mapSites := map[string]string{
+		"youtube": "http://youtube.com/ceuits",
+		"google": "http://google.com",
+		"yahoo": "http://yahoo.com/etusir",
+	}
+
+	mapSatusCode := map[string]int{}
+
+	for k, v := range mapSites{
+		_, statusCode, err := getStatusCode(v)
+		if err != nil {
+		log.Fatal(err)
+		}
+		fmt.Printf("URL : %v is %v [OK]\n", k, statusCode)
+		mapSatusCode[v] = statusCode
+	}
+
+	for k, v := range mapSatusCode {
+		err := checkStatusCode(v, k)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func getStatusCode(url string) (site string, statusCode int, err error){
+	site = url
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	statusCode = res.StatusCode
+	return
+}
+
+func checkStatusCode(code int, url string) (err error){
+	c := code
+	if c != okStatus {
+		err = fmt.Errorf("Error code is %v responding from %s", c, url)
+	}
+	return err
+}
