f9742bb7b1d0dc41290fc895e81db202292216fb
F00b4rch <f00b4rch@gmail.com>
2017-06-26 15:09:58 +0200

    Create nginx_acces_log_parser.sh

---
+++ bash/nginx_acces_log_parser.sh
@@ -0,0 +1,38 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: pars.sh
+# 
+#         USAGE: ./pars.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Corentin Deret (Administrateur Système DevOps)
+#       CREATED: 26/06/2017 10:38:54
+#      REVISION:  ---
+#===============================================================================
+
+file=$1
+
+# nombre total de requete
+totalReq=$(cat $file | wc -l)
+echo -ne "\ntotal request = $totalReq"
+
+# nombre total ip
+totalIp=$(grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $file | wc -l)
+echo -ne "\ntotal ip = $totalIp"
+
+cat $file | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > ip.txt
+echo -ne '\n\n****Top 10 IP****\n\n'
+sort ip.txt | uniq -c | sort -n -r | head -10
+rm ip.txt
+
+echo -ne '\n\n****Top 10 requetes*****\n\n'
+cat $file | awk '{print $7}' > requests
+sort requests | uniq -c | sort -n -r | head -10
+
+rm requests
