1f44700c82c7c258974bfb2add07eafe61303fc1
F00b4rch <f00b4rch@gmail.com>
2017-06-26 17:01:54 +0200

    Update nginx_acces_log_parser.sh

--- bash/nginx_acces_log_parser.sh
+++ bash/nginx_acces_log_parser.sh
@@ -23,10 +23,11 @@ totalReq=$(cat $file | wc -l)
 echo -ne "\ntotal request = $totalReq"
 
 # nombre total ip
-totalIp=$(grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $file | wc -l)
+totalIp=$(grep -Eo ':[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\ ' $file | wc -l)
 echo -ne "\ntotal ip = $totalIp"
 
-cat $file | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > ip.txt
+cat $file | grep -Eo ':[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\ ' > ip.txt
+sed -i 's/://g' ip.txt
 echo -ne '\n\n****Top 10 IP****\n\n'
 sort ip.txt | uniq -c | sort -n -r | head -10
 rm ip.txt
@@ -38,4 +39,4 @@ sort requests | uniq -c | sort -n -r | head -10
 rm requests
 
 echo -ne'\n\n***** REQUETES /h *****\n\n'
-for i in $(seq 0 9) ; do echo -ne "$i heures\n" ; grep "2017:0$i:" $file | wc -l ; done && for i in $(seq 10 23) ; do echo -ne "$i heures\n" ; grep "2017:$i:" $file | wc -l ; done
+for i in $(seq 0 9) ; do echo -ne "$i heures\n" ; grep "2017:0$i:" $file | wc -l ; done && for i in $(seq 10 23) ; do echo -ne "$i heures\n" ; grep "2017:$i:" $file | wc -l ; done 
