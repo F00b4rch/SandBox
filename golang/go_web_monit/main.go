79fb154f1166510817a8c1a5d19941f9d582c7a9
F00b4rch <f00b4rch@gmail.com>
2017-07-04 14:19:07 +0200

    Update main.go

--- golang/go_web_monit/main.go
+++ golang/go_web_monit/main.go
@@ -1,44 +1,64 @@
-// This program have one main
-// - Monitore website with http get status code answer
 package main
 
 import (
-	"net/http"
-	"log"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ashwanthkumar/slack-go-webhook"
 )
 
-// Define good answer from get
 const okStatus int = 200
 
 func main() {
 
-	mapSites := map[string]string{
-		"youtube": "http://youtube.com",
-		"google": "http://google.com",
-		"yahoo": "http://yahoo.com",
-	}
+	for {
 
-	mapSatusCode := map[string]int{}
-
-	for k, v := range mapSites{
-		_, statusCode, err := getStatusCode(v)
-		if err != nil {
-		log.Fatal(err)
+		mapURL := map[string]string{
+			"url1":          "https://www.test.com/1,
+			"url2":          "https://www.test.com/2",
 		}
-		fmt.Printf("URL : %v is %v [OK]\n", k, statusCode)
-		mapSatusCode[v] = statusCode
-	}
 
-	for k, v := range mapSatusCode {
-		err := checkStatusCode(v, k)
-		if err != nil {
-			log.Println(err)
+		mapStatusCode := map[string]int{}
+
+		for _, v := range mapURL {
+			_, statusCode, err := getStatusCode(v)
+			if err != nil {
+				payload("impossible to get status code" + err.Error())
+			}
+			mapStatusCode[v] = statusCode
 		}
+
+		for k, v := range mapStatusCode {
+			err := checkStatusCode(v, k)
+			if err != nil {
+				payload(err.Error())
+			}
+		}
+
+		time.Sleep(60 * time.Second)
+
 	}
 }
 
-func getStatusCode(url string) (site string, statusCode int, err error){
+func payload(text string) {
+
+	webhookURL := "https://hooks.slack.com/services/WEBHOOK"
+
+	payload := slack.Payload{
+		Text:     text,
+		Username: "test,
+		Channel:  "#infra",
+	}
+	err := slack.Send(webhookURL, "", payload)
+	if len(err) > 0 {
+		fmt.Printf("error : %s\n", err)
+	}
+
+}
+
+func getStatusCode(url string) (site string, statusCode int, err error) {
 	site = url
 	res, err := http.Get(url)
 	if err != nil {
@@ -48,7 +68,7 @@ func getStatusCode(url string) (site string, statusCode int, err error){
 	return
 }
 
-func checkStatusCode(code int, url string) (err error){
+func checkStatusCode(code int, url string) (err error) {
 	c := code
 	if c != okStatus {
 		err = fmt.Errorf("Error code is %v responding from %s", c, url)
