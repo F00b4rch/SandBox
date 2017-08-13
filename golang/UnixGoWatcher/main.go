084ba9416f9343e7bf13c698a5add6412654b01f
F00b4rch <f00b4rch@gmail.com>
2017-08-13 14:55:23 +0200

    adding first ex

---
+++ golang/UnixGoWatcher/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/load"
+)
+
+func main() {
+
+	// Get current Load
+	avg, err := load.Avg()
+	if err != nil {
+		log.Printf("error while getting average %s", err)
+	}
+	if avg.Load1 > 5 {
+		fmt.Printf("Waring, load average > 5 : actual load is %v", avg.Load1)
+	}
+
+	// Get Kernel version
+	v, err := host.KernelVersion()
+	if err != nil {
+		log.Printf("error while getting kernel version %s", err)
+	}
+	fmt.Printf("Kernel version %s \n", v)
+
+	// Get users
+	u, err := host.Users()
+	if err != nil {
+		log.Printf("error while getting users %s", err)
+	}
+	for _, v := range u {
+		fmt.Print(v.User)
+	}
+}
