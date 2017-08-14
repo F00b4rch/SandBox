8914fcbd90eda56ae64ea0deb9acf5210ec56e04
F00b4rch <f00b4rch@gmail.com>
2017-08-14 14:38:45 +0200

    Update main.go

--- golang/UnixGoWatcher/main.go
+++ golang/UnixGoWatcher/main.go
@@ -1,37 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"strconv"
 
+	"github.com/F00b4rch/UnixGoWatch/slackApp"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/load"
 )
 
 func main() {
+	// Get current hostname
+	v, err := host.Info()
+	if err != nil {
+		log.Printf("error while getting hostname %s", err)
+	}
 
 	// Get current Load
 	avg, err := load.Avg()
 	if err != nil {
 		log.Printf("error while getting average %s", err)
 	}
-	if avg.Load1 > 5 {
-		fmt.Printf("Warning, load average > 5 : actual load is %v", avg.Load1)
-	}
-
-	// Get Kernel version
-	v, err := host.KernelVersion()
-	if err != nil {
-		log.Printf("error while getting kernel version %s", err)
-	}
-	fmt.Printf("Kernel version %s \n", v)
-
-	// Get users
-	u, err := host.Users()
-	if err != nil {
-		log.Printf("error while getting users %s", err)
-	}
-	for _, v := range u {
-		fmt.Print(v.User)
+	if avg.Load1 > 4 {
+		slackApp.PayloadSlack("[ALERTE] host : " + v.Hostname + ", load average > 4 : actual load " + strconv.FormatFloat(avg.Load1, 'f', 2, 64))
 	}
 }
