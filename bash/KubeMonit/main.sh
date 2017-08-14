393ee62a77310db2a0e04064c3b880b88220f341
F00b4rch <f00b4rch@gmail.com>
2017-08-14 16:49:20 +0200

    Create main.sh

---
+++ bash/KubeMonit/main.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+#/ Usage: ./main.sh
+#/ Description: This script monitore kubernetes infrastructure
+#/ Examples: ./main.sh
+#/ Options: 
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+expr "$*" : ".*--help" > /dev/null && usage
+
+readonly LOG_FILE="/tmp/$(basename "$0").log"
+info()    { echo "$(date -u) [INFO]    $*" | tee -a "$LOG_FILE" >&2 ; }
+warning() { echo "$(date -u) [WARNING] $*" | tee -a "$LOG_FILE" >&2 ; }
+error()   { echo "$(date -u) [ERROR]   $*" | tee -a "$LOG_FILE" >&2 ; }
+fatal()   { echo "$(date -u) [FATAL]   $*" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
+
+#/ sourcing vars:
+source config.sh
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+   
+    #/ If root : 
+	if [[ $EUID -eq 0 ]]; then
+    	echo "This script must be run as root" 
+    	exit 1
+    fi
+    
+	#/ Func getting Nodes
+    getNodes() {
+		currentNodes=$(kubectl get nodes | grep -Ec '^gke')
+		if [[ "$currentNodes" = "$sumNodes" ]]; then
+			info "Number of nodes gets $currentNodes wanted $sumNodes"
+		else
+			warning "Number of nodes mismatch, wanted $sumNodes, have $currentNodes"
+		fi
+	}
+
+	getNodes
+
+fi			
