a385b95538a2f976e0d4d5ef2e6f1f6f37cd46b3
F00b4rch <f00b4rch@gmail.com>
2017-08-18 16:53:08 +0200

    Update main.py

--- python/staytus/main.py
+++ python/staytus/main.py
@@ -27,22 +27,28 @@ def main():
     # offline
 
     for k, value in urls.items():
-        req_site = requests.head(k)
-        total_sec = requests.get(k, timeout=30).elapsed.total_seconds()
-
-        if req_site.status_code != 200:
-            params = {'service': value, 'status':'degraded-performance'}
-            requests.get(ndd + url, headers=headers, json=params)
-        elif req_site.status_code == 200:
-            params = {'service': value, 'status':'operational'}
-            requests.get(ndd + url, headers=headers, json=params)
-        else:
-            pass
-
-        if total_sec > 30:
-            params = {'service': value, 'status':'degraded-performance'}
-            requests.get(ndd + url, headers=headers, json=params)
-
+        try:
+        
+            req_site = requests.head(k)
+            total_sec = requests.get(k, timeout=30).elapsed.total_seconds()
+            
+            if req_site.status_code != 200:
+                params = {'service': value, 'status':'degraded-performance'}
+                requests.get(ndd + url, headers=headers, json=params)
+            elif req_site.status_code == 200:
+                params = {'service': value, 'status':'operational'}
+                requests.get(ndd + url, headers=headers, json=params)
+            else:
+                pass
+            
+            if total_sec > 30:
+                params = {'service': value, 'status':'degraded-performance'}
+                requests.get(ndd + url, headers=headers, json=params)
+        
+        except Exception as x:
+            print(x)
+            
 while True:
     main()
     time.sleep(5)
+    
