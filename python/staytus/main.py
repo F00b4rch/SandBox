e377d1407f25cc54616535e7dedf257938702dde
F00b4rch <f00b4rch@gmail.com>
2017-08-21 10:00:27 +0200

    Update main.py

--- python/staytus/main.py
+++ python/staytus/main.py
@@ -8,15 +8,16 @@ def main():
     """Main content"""
 
     # Website
-    urls = {'http://website1.com' : 'website1-com',
-            'http://website2.com' : 'website2-com',
-            'http://website3.com' : 'website3-com'}
+    urls = {'http://my.site1.com' : 'my-site1-com',
+            'http://my.site2.com' : 'my-site2-com',
+            'http://my.site3.com' : 'my-site2-com'
+           }
 
     # Staytus
-    ndd = "https://status.website.com"
+    ndd = "https://status.my-site.com"
     url = "/api/v1/services/set_status"
-    headers = {'X-Auth-Token': 'XXXXXXXXXXXXXX',
-               'X-Auth-Secret': 'XXXXXXXXXXXXXXX'}
+    headers = {'X-Auth-Token': 'XXX-XXX-XXX-XXX-XXX',
+               'X-Auth-Secret': 'xxxxxxxxxxxxxxxxxxxxx'}
 
     # -- List of statuses --
     # operational
@@ -27,11 +28,11 @@ def main():
     # offline
 
     for k, value in urls.items():
+
         try:
-        
             req_site = requests.head(k)
             total_sec = requests.get(k, timeout=30).elapsed.total_seconds()
-            
+
             if req_site.status_code != 200:
                 params = {'service': value, 'status':'degraded-performance'}
                 requests.get(ndd + url, headers=headers, json=params)
@@ -40,15 +41,17 @@ def main():
                 requests.get(ndd + url, headers=headers, json=params)
             else:
                 pass
-            
+
             if total_sec > 30:
                 params = {'service': value, 'status':'degraded-performance'}
                 requests.get(ndd + url, headers=headers, json=params)
-        
-        except Exception as x:
-            print(x)
-            
+            else:
+                params = {'service': value, 'status':'operational'}
+                requests.get(ndd + url, headers=headers, json=params)
+
+        except requests.RequestException as err:
+            print(err)
+
 while True:
     main()
     time.sleep(5)
-    
