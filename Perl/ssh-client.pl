c4f69babdd2ed69f6d2218ec895bba0084746ab6
F00b4rch <f00b4rch@gmail.com>
2018-09-09 16:40:33 +0200

    Create ssh-client.pl

---
+++ Perl/ssh-client.pl
@@ -0,0 +1,44 @@
+#!/bin/perl
+# dev : f00b4rch
+# description : First Perl program
+
+# Use part
+use warnings;
+use strict;
+use Net::SSH::Perl;
+
+# Starting
+print("Initialize connexion...\n");
+
+=begin
+Please see full documentation here : 
+- https://metacpan.org/pod/Net::SSH::Perl
+
+Exemple vars :
+$host       // IP-host
+$username   // ssh username
+$password   // ssh password
+$port       // ssh port
+=cut
+
+my $host = $ENV{'HOST'};;
+my $username = $ENV{'USERNAME'};
+my $password = $ENV{'PASSWORD'};
+
+print($host);
+my $ssh = Net::SSH::Perl->new($host);
+print("Connexion success!\nBegin authentification...");
+
+$ssh->login($username,$password);
+
+# Defining 
+my $command = "w";
+my($stdout, $stderr, $exit) = $ssh->cmd('$command');
+# Checking error
+if ($stderr ne "")
+{
+    print('Error while executing $command : $stderr');
+}
+print $stdout;
+
+exit;
